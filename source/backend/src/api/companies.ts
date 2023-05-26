883c86ecfb45185390196fa62b90e0d1a2d21463
melogo <i.plyton@gmail.com>
2023-05-26 20:25:24 +0500

    companies api

---
+++ source/backend/src/api/companies.ts
@@ -0,0 +1,32 @@
+import type { FastifyInstance } from "fastify";
+import type DB from "../../class/DB";
+import { companiesList, companyCreate } from "../service/companies";
+import { CompanyModel } from "@app/types/model/company";
+
+const companiesApi = (fastify: FastifyInstance, options: { db: DB }, done: () => void): void => {
+  const { db } = options;
+
+  /** Получение списка всех компаний */
+  fastify.post("/companies/list", async (request, reply) => {
+    try {
+      const items = await companiesList(db);
+      return { items };
+    } catch (error) {
+      //
+    }
+  });
+
+  /** Создание новой компании */
+  fastify.post("/companies/create", async (request, reply) => {
+    const eventData = JSON.parse(request.body as string) as CompanyModel;
+    const id = await companyCreate(eventData, db);
+    return {
+        ...eventData,
+        id
+    };
+  });
+
+  done();
+}
+
+export default companiesApi;
---
+++ source/types/model/company.ts
@@ -0,0 +1,7 @@
+import type { UUID } from "node:crypto";
+
+export type CompanyModel = {
+    id?: UUID;
+    name: string;
+    description: string;
+}
