cac29847f56f3c79d1a31b5e28475e8de4074a8a
melogo <i.plyton@gmail.com>
2023-05-27 13:38:14 +0500

    candidate model

--- source/types/model/candidate.ts
+++ source/types/model/candidate.ts
@@ -11,6 +11,9 @@ export type CreateCandidateModel = {
     email: string;
     phone: string;
     telegram: string;
+    link: string; // ссылка на страницу где-нибудь в хедхантере и тд
+    file: string, // путь до файла с резюме
+    photo: string; // путь до фотки
     vk: string;
     notes: string;
     department: string;
