d9ae1d386b0c9adb91ca43d7c24da798b6d4695c
Potashin Andrey <potashin@ro.ru>
2023-05-27 11:54:31 +0300

    Изменение для формы добавления кандидата

--- source/frontend/src/pages/CandidateCreate.tsx
+++ source/frontend/src/pages/CandidateCreate.tsx
@@ -1,10 +1,10 @@
 import type { FC } from "react";
 import { useForm } from 'effector-react-form';
 import { useUnit } from "effector-react";
-import { Button } from "antd";
+import { Button, Divider, Typography  } from "antd";
 
 import { candidateCreateForm, candidateCreateFormSubmit, $newCandidate } from '../context/candidate';
-import { InputField, TextAreaField } from '../form/input';
+import { DocsUploadField, InputField, TextAreaField } from '../form/input';
 import { SelectField } from '../form/select';
 import { RadioField } from '../form/radio';
 import { DatePickerField } from "../form/datePicker";
@@ -40,6 +40,8 @@ const DEPARTMENTS = [{
     value: "dataAnalysis"
 }];
 
+const { Title } = Typography;
+
 const CandidateCreate: React.FC = () => {
     const { controller, handleSubmit } = useForm({ form: candidateCreateForm });
     const values = useUnit(candidateCreateForm.$values)
@@ -50,7 +52,17 @@ const CandidateCreate: React.FC = () => {
 
     return (
         <div className="candidateCreatePage boxAndRadius">
+            <Title level={4}>Основная информация</Title>
             <InputField controller={controller({ name: "name" })} label={"Имя"} />
+            <DocsUploadField controller={controller({ name: "photo" })} label={"Фото кандидата"} />
+
+            <RadioField controller={controller({ name: "sex" })} label={"Пол"} options={SEX} />
+            <DatePickerField controller={controller({ name: "birthDate" })} label={"День Рожденья"} placeholder="Выберете дату" />
+            <SelectField controller={controller({ name: "department" })} label={"Департамент"} options={DEPARTMENTS} />
+            
+            <Divider />
+            <Title level={4}>Профессиональные навыки</Title>
+
             <InputField controller={controller({ name: "position" })} label={"Позиция"} />
             <InputField controller={controller({ name: "salary" })} label={"Зарплата"} />
             <InputField controller={controller({ name: "experience" })} label={"Опыт"} />
@@ -58,10 +70,14 @@ const CandidateCreate: React.FC = () => {
             <InputTagField controller={controller({ name: "tags" })} label={"Теги"} />
             <InputTagField controller={controller({ name: "skills" })} label={"Навыки"} />
 
-            <RadioField controller={controller({ name: "sex" })} label={"Пол"} options={SEX} />
-            <DatePickerField controller={controller({ name: "birthDate" })} label={"День Рожденья"} placeholder="Выберете дату" />
-            <TextAreaField controller={controller({ name: "descriptionText" })} label={"Описание"} />
+            <Divider />
+            <Title level={4}>Стороние резюме</Title>
+            <InputField controller={controller({ name: "link" })} label={"Ссылка на сторонее резюме"} />
+            <DocsUploadField controller={controller({ name: "file" })} label={"Файл с резюме"} />
 
+            <Divider />
+
+            <Title level={4}>Контакты кандидата</Title>
             <div className="boxAndRadius candidateCreatePageContacts" >
                 <h5>Контакты</h5>
                 <InputField controller={controller({ name: "email" })} label={"Почта"} />
@@ -70,8 +86,10 @@ const CandidateCreate: React.FC = () => {
                 <InputField controller={controller({ name: "vk" })} label={"ВКонтакте"} />
             </div>
 
+            <Divider />
+
             <TextAreaField controller={controller({ name: "notes" })} label={"Заметки"} />
-            <SelectField controller={controller({ name: "department" })} label={"Департамент"} options={DEPARTMENTS} />
+            <TextAreaField controller={controller({ name: "descriptionText" })} label={"Описание"} />
 
             <Button onClick={candidateCreateFormSubmit}>Создать</Button>
         </div>
