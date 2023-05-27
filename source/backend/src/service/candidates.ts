845b9a41c7a6b6ff3a6b132dad50c407d5fdb360
Potashin Andrey <potashin@ro.ru>
2023-05-27 16:59:55 +0300

    birthdate

--- source/backend/src/service/candidates.ts
+++ source/backend/src/service/candidates.ts
@@ -10,11 +10,11 @@ export const candidatesList = async (db: DB): Promise<Array<any>> => {
 }
 
 export const createCandidate = async (values: CandidateModel, db: DB): Promise<UUID> => {
-    const { name, birthDate, description, email, experience, file, link, notes, phone, photo, position, salary, sex, skills, tags, telegram, vacancy, vk } = values;
+    const { name, birthdate, description, email, experience, file, link, notes, phone, photo, position, salary, sex, skills, tags, telegram, vacancy, vk } = values;
     return await db.insert({
-        fields: "vacancy, name, birthDate, description, email, experience, file, link, notes, phone, photo, position, salary, sex, skills, tags, telegram, vk",
+        fields: "vacancy, name, birthdate, description, email, experience, file, link, notes, phone, photo, position, salary, sex, skills, tags, telegram, vk",
         tables: "service.applicants",
-        values: [vacancy, name, birthDate, description, email, experience, file, link, notes, phone, photo, position, salary, sex, skills, tags, telegram, vk],
+        values: [vacancy, name, birthdate, description, email, experience, file, link, notes, phone, photo, position, salary, sex, skills, tags, telegram, vk],
         returning: "id"
     });
 }
